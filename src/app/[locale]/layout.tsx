57dabaa54ccf86f1c715a6d4c31cc47900d6259d
水煮油条 <105115833+zhuzaiBro@users.noreply.github.com>
2026-06-02 16:16:55 +0800

    Update layout.tsx

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -57,6 +57,14 @@ export default async function RootLayout({
 
   return (
     <html lang={locale} suppressHydrationWarning className="scrollbar-hide">
+
+      <head>
+        <meta
+          httpEquiv="Content-Security-Policy"
+          content="upgrade-insecure-requests"
+        />
+      </head>
+      
       <body className={`${dinpro.variable} bg-[#111319] font-sans`}>
         <NextIntlClientProvider messages={messages}>
           <ThemeProvider defaultTheme="light">
